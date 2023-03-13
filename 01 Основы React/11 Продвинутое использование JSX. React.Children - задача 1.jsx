b91f24184e5ae94d78d0fb45f4a8ae13690589db
artazeon <artazeon@gmail.com>
2023-03-13 15:49:09 +0300

    11 Продвинутое использование JSX. React.Children - задача 1

---
+++ 01 Основы React/11 Продвинутое использование JSX. React.Children - задача 1.jsx
@@ -0,0 +1,18 @@
+const Quote = (props) => {
+  return (
+    <div className="fancy-quote">
+      <p className='text'>{props.children}</p> {/* Переписать на props.children */}
+      <span className='author'>{props.author}</span>
+    </div>
+  );
+}
+
+
+const DuneQuote = () => {
+  return ( 
+    <Quote
+      author="Принцесса Ирулан «Избранные&nbsp;изречения Муад'Диба»">Арракис учит «пониманию ножа», учит относиться к жизни так: отрезать всё несовершенное и незавершённое, говоря: «Вот теперь это совершенно и завершено — ибо кончается здесь».</ Quote>
+  );
+}
+
+ReactDOM.render(<DuneQuote />, document.querySelector('#root'));
