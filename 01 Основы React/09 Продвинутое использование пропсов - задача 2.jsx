69161db3f4a32cec101abfc5f3ea53cd967bae20
artazeon <artazeon@gmail.com>
2023-03-13 11:34:10 +0300

    Продвинутое использование пропсов - задача 2

--- 01 Основы React/09 Продвинутое использование пропсов - задача 2.jsx
+++ 01 Основы React/09 Продвинутое использование пропсов - задача 2.jsx
@@ -24,7 +24,7 @@ const meals = [
 
 class App extends React.Component {
   randomMeal(arr) {
-   return arr[Math.floor(Math.random() * 6)]
+   return arr[Math.floor(Math.random() * (arr.length-1))]
   }
 
   render() {
