23877d98396d606bfbf2cb859c3251fb5a6922b0
artazeon <artazeon@gmail.com>
2023-03-13 20:21:07 +0300

    12 Внутреннее состояние компонентов - задача 1

---
+++ 01 Основы React/12 Внутреннее состояние компонентов - задача 1.jsx
@@ -0,0 +1,26 @@
+class Employee extends React.Component {
+  state = {
+    age: 65,
+    avatar: "https://code.s3.yandex.net/react/code/albert-einstein.jpg",
+    name: "Альберт Энштейн",
+    address: {
+      country: "Германия",
+      city: "Ульм",
+    },
+    favoriteTechnology: "React.js",
+  };
+
+  render() {
+    return (
+      <div className="card">
+        <img className="card__image" alt="аватар" src={this.state.avatar} />
+        <h4 className="card__name">Имя: {this.state.name}</h4>
+        <p className="card__text">Полных лет: {this.state.age}</p>
+        <p className="card__text">Место пребывания: {this.state.address.country + ', ' + this.state.address.city}</p>
+        <p className="card__text">Любимая технология: {this.state.favoriteTechnology}</p>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Employee />, document.querySelector("#root"));
