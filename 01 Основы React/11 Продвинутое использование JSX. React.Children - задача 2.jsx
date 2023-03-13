77ec295280f7d442ea0dde56583ce9c3b8ad4e8b
artazeon <artazeon@gmail.com>
2023-03-13 16:21:40 +0300

    11 Продвинутое использование JSX. React.Children - задача 2

---
+++ 01 Основы React/11 Продвинутое использование JSX. React.Children - задача 2.jsx
@@ -0,0 +1,29 @@
+const MenuBlock = props => {
+  return (
+    <div className="menu">
+      <div className="content">
+        <p className="menu__title">{props.menuHeading}</p>
+        {props.children}
+      </div>
+    </div>
+  );
+};
+
+const Footer = () => {
+
+  return (
+    <div className="footer">
+      <div className="container">
+        <img className="menu__img" src="./images/bg.svg" alt="Подвал" />
+        <MenuBlock menuHeading={"О компании"}><ul className="nav">
+      <li className="nav__link">Наша миссия</li>
+      <li className="nav__link">Вакансии</li>
+      <li className="nav__link">Блог</li>
+    </ul></MenuBlock>
+
+      </div>
+    </div>
+  );
+};
+
+ReactDOM.render(<Footer />, document.querySelector('#root'));
