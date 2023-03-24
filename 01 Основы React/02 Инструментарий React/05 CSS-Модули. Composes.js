26548cf64039053c8a5c062a509f888691b140f8
artazeon <artazeon@gmail.com>
2023-03-24 17:37:37 +0300

    05 CSS-Модули. Composes

---
+++ 01 Основы React/02 Инструментарий React/05 CSS-Модули. Composes.js
@@ -0,0 +1,26 @@
+// //app.js
+
+
+
+
+// //main.js
+
+
+// //header.js
+
+// import React from 'react';
+// import headerLogo from '../../images/logo.png';
+// import headerStyles from './header.module.css';
+
+// class Header extends React.Component {
+//   render() {
+//     return (
+//       <header className={headerStyles.header}>
+//         <img className={headerStyles.logo} src={headerLogo} alt="логотип." />
+//         <h1 className={headerStyles.title}>Собачки, следящие за тобой, пока ты изучаешь React</h1>
+//       </header>
+//     );
+//   }
+// }
+
+// export default Header;
