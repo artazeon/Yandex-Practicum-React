0d1a5fd931f5b34a06b601cdebd1f0e2e25dfa8a
artazeon <artazeon@gmail.com>
2023-03-25 18:13:17 +0300

    05 CSS-Модули. Composes - задача 2

---
+++ 01 Основы React/02 Инструментарий React/05 CSS-Модули. Composes - задача  2.js
@@ -0,0 +1,49 @@
+// //app.js
+
+
+
+
+// //main.js
+
+// import React from 'react';
+// import mainStyles from './main.module.css';
+
+// class Main extends React.Component {
+//   render() {
+//     const { image, caption, title, description } = this.props.mainData;
+    
+//     return (
+//       <main className={mainStyles.main}>
+//         <img className={mainStyles.image} src={image} alt="фото собачек." />
+//         <span className={mainStyles.caption}>{caption}</span>
+//         <h2 className={mainStyles.title}>{title}</h2>
+//         <p className={mainStyles.description}>{description}</p>
+//       </main>
+//     );
+//   }
+// }
+
+// export default Main;
+
+
+// //header.js
+
+
+// //header.module.css
+// .header {
+//     width: 100%;
+//     display: flex;
+//     align-items: center;
+//   }
+  
+//   .logo {
+//     width: 50px;
+//     height: 50px;
+//     margin-right: 30px;
+//   }
+  
+//   .title {
+//     font-size: 36px;
+//     line-height: 1.15;
+//     font-weight: 500;
+//   }
