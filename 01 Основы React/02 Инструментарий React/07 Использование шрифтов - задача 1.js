ee114040648c40a0ea31f5c73fb8ce889b209ce0
artazeon <artazeon@gmail.com>
2023-03-25 18:45:52 +0300

    07 Использование шрифтов - задача 1

---
+++ 01 Основы React/02 Инструментарий React/07 Использование шрифтов - задача 1.js
@@ -0,0 +1,47 @@
+// //index.css
+// @font-face {
+//     font-family: 'Spectral';
+//     font-style: normal;
+//     font-weight: 700;
+//     src: url("./fonts/spectral.woff") format("woff");
+//   }
+  
+//   @font-face {
+//     font-family: 'Spectral';
+//     font-style: normal;
+//     font-weight: 300;
+//     src: url("./fonts/spectral.woff") format("woff");
+//   }
+  
+//   @font-face {
+//     font-family: 'Spectral';
+//     font-weight: 300;
+//     font-style: italic;
+//     src: url("./fonts/spectral.woff") format("woff");
+//   }
+
+
+
+
+// //header.module.css
+// .header {
+//     width: 100%;
+//     display: flex;
+//     align-items: center;
+//   }
+  
+//   .logo {
+//     width: 64px;
+//     height: 64px;
+//     margin-right: 16px;
+//     margin-bottom: 20px;
+//   }
+  
+//   .title {
+//     font-size: 32px;
+//     line-height: 1;
+//     font-weight: 700;
+//     margin: 0 0 20px;
+//     font-family: 'Spectral', Arial, sans-serif
+//   }
+
