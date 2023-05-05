40710228d530257e835293e70cf71289b60573a8
artazeon <artazeon@gmail.com>
2023-05-05 15:18:43 +0300

    Ref Доступ к DOM из React

---
+++ 01 Основы React/03 Продвинутый React/03 Ref Доступ к DOM из React - задача 2.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import formStyles from './form.module.css';
+
+class Form extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isSubmit: false,
+      value: ''
+    };
+
+    this.emailInput = React.createRef();
+    this.handleFormSubmit = this.handleFormSubmit.bind(this);
+  }
+
+  componentDidMount() {
+    this.emailInput.current.focus();
+  }
+
+handleFormSubmit(e) {
+    e.preventDefault();
+    this.setState({
+      ...this.state,
+      isSubmit: true,
+      value: this.emailInput.current.value
+    });
+  } 
+
+  render() {
+    return (
+      <div className={formStyles.root}>
+        <form
+          className={formStyles.form}
+          onSubmit={this.handleFormSubmit}
+        >
+          <input
+            ref={this.emailInput}
+            className={formStyles.input}
+            type='email'
+            placeholder={'Введите свой e-mail'}
+          />
+          <button
+            className={formStyles.button}
+            type={'submit'}
+          >
+            Подписаться
+          </button>
+        </form>
+<p className={formStyles.message}>
+  {
+    this.state.isSubmit && this.state.value &&
+    <>
+      <span>На почту {this.state.value} успешно подписана рассылка</span>
+      <span>Но это не точно</span>
+    </>
+  }
+</p> 
+      </div>
+    );
+  }
+}
+
+export default Form;
