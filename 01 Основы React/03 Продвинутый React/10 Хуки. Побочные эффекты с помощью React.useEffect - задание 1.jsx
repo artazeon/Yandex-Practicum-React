d0c3738157f0b01360a1780bd30c505a89de8377
artazeon <artazeon@gmail.com>
2023-05-17 23:35:16 +0300

    Хуки. Побочные эффекты с помощью React.useEffect

---
+++ 01 Основы React/03 Продвинутый React/10 Хуки. Побочные эффекты с помощью React.useEffect - задание 1.jsx
@@ -0,0 +1,28 @@
+function toggleBackground(shouldShow) {
+  document.body.classList.toggle('with-bg', shouldShow);
+}
+
+function Beautifier() {
+  const [isBeautiful, setIsBeautiful] = React.useState(false);
+
+  React.useEffect(
+  () => {
+    toggleBackground(isBeautiful)
+  }
+  );
+
+  function handleChange() {
+    setIsBeautiful(!isBeautiful);
+  }
+
+  return (
+    <label>
+      <input type="checkbox" onChange={handleChange} />
+      Включить красивый фон
+    </label>
+  );
+}
+
+ReactDOM.render((
+  <Beautifier />
+), document.querySelector('#root'));
---
+++ 01 Основы React/03 Продвинутый React/10 Хуки. Побочные эффекты с помощью React.useEffect - задание 2.jsx
@@ -0,0 +1,55 @@
+function playSound(fileName) {
+  const audio = new Audio();
+  audio.src = `https://code.s3.yandex.net/web-code/react/${fileName}`;
+  audio.play();
+}
+
+function App() {
+  const [isTimeShown, setIsTimeShown] = React.useState();
+
+  function handleChange() {
+    setIsTimeShown(!isTimeShown);
+  }
+
+  return (
+    <>
+      <label>
+        <input type="checkbox" onChange={handleChange}/>
+        Включить время (осторожно, звук!)
+      </label>
+      {isTimeShown && <Time/>}
+    </>
+  );
+}
+
+function Time() {
+  const [hours, setHours] = React.useState(0);
+  const [minutes, setMinutes] = React.useState(0);
+  const [seconds, setSeconds] = React.useState(0);
+
+  React.useEffect(() => {
+    const interval = setInterval(() => {
+      const date = new Date();
+      setSeconds(date.getSeconds());
+      setMinutes(date.getMinutes());
+      setHours(date.getHours());
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  React.useEffect(() => {
+    playSound('tick.mp3');
+  }, [seconds]);
+  
+    React.useEffect(() => {
+    playSound('gong.mp3');
+  }, []);
+
+  return (
+    <h2>{hours}:{minutes}:{seconds}</h2>
+  );
+}
+
+ReactDOM.render((
+  <App/>
+), document.querySelector('#root'));
